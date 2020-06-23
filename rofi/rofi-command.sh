ad6eacb849ea5d2bde7b386d4bb0b52fce03bf1f
Evgeni Tsvetanov <etsvet@softserveinc.com>
2020-06-24 00:26:06 +0300

    Add rofi command and all of its arguments

---
+++ rofi/rofi-command.sh
@@ -0,0 +1,1 @@
+rofi -combi-modi window,chrome:~/.config/rofi/chrome-switch-tabs.sh -show combi -modi combi,run -icon-theme "Mojave-CT-Classic" -show-icons -opacity 10 -monitor primary -font mono\ 16
